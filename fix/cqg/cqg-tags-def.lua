71928cb9f334cf67fb2b771b5cb7bdfc394fd625
Richard Gomes <rgomes.info@gmail.com>
2016-08-02 17:24:11 +0100

    First import.

---
+++ fix/cqg/cqg-tags-def.lua
@@ -0,0 +1,16 @@
+--
+-- Defines QuickFix tags from copied/pasted text from documentation organized as a CSV file.
+--
+--   input looks like:  "20010","AccountName","String","N","Symbolic name of account."
+--   output looks like: <field number="20010" name ="AccountName" type="STRING" />
+--
+
+prev=0
+for line in io.input():lines() do
+  for tag_, name_, type_, len_, req_ in string.gmatch(line, '"(%w+)","(%w+)","(%w+)(%(*%d*%)*)","(%w+)"') do
+    if (tag_ ~= prev) then
+      print('  <field number="' .. tag_ .. '" name ="' .. name_ .. '" type="' .. string.upper(type_) .. '" />')
+    end
+    prev = tag_
+  end
+end
---
+++ fix/cqg/cqg-tags-ref.lua
@@ -0,0 +1,16 @@
+--
+-- Make reference to QuickFIX tags from copied/pasted from documentation organized as a CSV file.
+--
+--   input looks like:  "20010","AccountName","String","N","Symbolic name of account."
+--   output looks like: <field name="AccountName" required="N" />
+--
+
+prev=0
+for line in io.input():lines() do
+  for tag_, name_, type_, len_, req_ in string.gmatch(line, '"(%w+)","(%w+)","(%w+)(%(*%d*%)*)","(%w+)"') do
+    if (tag_ ~= prev) then
+      print('  <field name="' .. name_ .. '" required="N" />')
+    end
+    prev=tag_
+  end
+end
